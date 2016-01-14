865e8936c4393878c1f4dedc0818c82db9b4e453
Patryk <patryk@kowalczyk.ws>
2016-01-14 20:06:29 +0100

    usb: chipidea: usbmisc_imx: add clock information for udoo

--- drivers/usb/chipidea/usbmisc_imx.c
+++ drivers/usb/chipidea/usbmisc_imx.c
@@ -11,6 +11,7 @@
 
 #include <linux/module.h>
 #include <linux/of_platform.h>
+#include <linux/clk.h>
 #include <linux/err.h>
 #include <linux/io.h>
 #include <linux/delay.h>
@@ -92,6 +93,7 @@ struct usbmisc_ops {
 
 struct imx_usbmisc {
 	void __iomem *base;
+	struct clk *clk;
 	spinlock_t lock;
 	const struct usbmisc_ops *ops;
 };
@@ -512,6 +514,7 @@ static int usbmisc_imx_probe(struct platform_device *pdev)
 {
 	struct resource	*res;
 	struct imx_usbmisc *data;
+	int ret;
 	const struct of_device_id *of_id;
 
 	of_id = of_match_device(usbmisc_imx_dt_ids, &pdev->dev);
@@ -529,6 +532,20 @@ static int usbmisc_imx_probe(struct platform_device *pdev)
 	if (IS_ERR(data->base))
 		return PTR_ERR(data->base);
 
+	data->clk = devm_clk_get(&pdev->dev, NULL);
+ 	if (IS_ERR(data->clk)) {
+ 		dev_err(&pdev->dev,
+ 			"failed to get clock, err=%ld\n", PTR_ERR(data->clk));
+ 		return PTR_ERR(data->clk);
+ 	}
+ 
+ 	ret = clk_prepare_enable(data->clk);
+ 	if (ret) {
+ 		dev_err(&pdev->dev,
+ 			"clk_prepare_enable failed, err=%d\n", ret);
+ 		return ret;
+ 	}
+ 
 	data->ops = (const struct usbmisc_ops *)of_id->data;
 	platform_set_drvdata(pdev, data);
 
@@ -536,7 +553,9 @@ static int usbmisc_imx_probe(struct platform_device *pdev)
 }
 
 static int usbmisc_imx_remove(struct platform_device *pdev)
-{
+{	
+	struct imx_usbmisc *usbmisc = dev_get_drvdata(&pdev->dev);
+	clk_disable_unprepare(usbmisc->clk);
 	return 0;
 }
 
